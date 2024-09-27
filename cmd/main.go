fa7af76a8b01779afa06b5af9c73d05a836a72f5
AlejandroLayos <alejandro.layos@gmail.com>
2024-09-27 15:44:26 +0200

    Add files via upload

--- cmd/main.go
+++ cmd/main.go
@@ -32,7 +32,7 @@ func main() {
 
 	// CORS configuration
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Change this to your frontend's origin
+		AllowOrigins:     []string{"http://localhost:4173"}, // Change this to your frontend's origin
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
